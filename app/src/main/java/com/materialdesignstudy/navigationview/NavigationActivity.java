80ff11bfe7f803b81348416e319bd0b6c75d84cf
HLQ <925954424@qq.com>
2017-10-20 23:51:18 +0800

    新增DrawerLayout+NavigationView实现侧滑 并添加点击事件

--- app/src/main/java/com/materialdesignstudy/navigationview/NavigationActivity.java
+++ app/src/main/java/com/materialdesignstudy/navigationview/NavigationActivity.java
@@ -1,15 +1,50 @@
 package com.materialdesignstudy.navigationview;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import com.materialdesignstudy.R;
 
 public class NavigationActivity extends AppCompatActivity {
 
+    private NavigationView mNavigationView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navugation);
+        initView();
+    }
+
+    private void initView() {
+        mNavigationView = findViewById(R.id.id_nv);
+        View headView = mNavigationView.getChildAt(0);
+        headView.findViewById(R.id.id_nv_head_img).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(NavigationActivity.this, "点击了头像", Toast.LENGTH_SHORT).show();
+            }
+        });
+        headView.findViewById(R.id.id_nv_head_username).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(NavigationActivity.this, "点击了用户名称", Toast.LENGTH_SHORT).show();
+            }
+        });
+        // 设置item选中事件
+        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                if (menuItem.getItemId() == R.id.action_call_me) {
+                    Toast.makeText(NavigationActivity.this, "当前点击了给我打电话", Toast.LENGTH_SHORT).show();
+                }
+                Toast.makeText(NavigationActivity.this, "当前点击了：" + menuItem.getTitle().toString(), Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/materialdesignstudy/snackbar/SnackBarActivity.java
@@ -0,0 +1,19 @@
+package com.materialdesignstudy.snackbar;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.materialdesignstudy.R;
+
+/**
+ * create by heliquan at 2017年10月20日
+ * Snackbar 介于Toast和Dialog之间
+ */
+public class SnackBarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_snack_bar);
+    }
+}
